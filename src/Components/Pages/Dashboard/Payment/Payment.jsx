6e1dbe25040b21998e960fa1f657be0d7d87c3e3
hasankarim18 <hallysust@gmail.com>
2023-06-04 23:15:22 +0600

    create checkout form and setting page ui for @stripe/react-stripe-js

--- src/Components/Pages/Dashboard/Payment/Payment.jsx
+++ src/Components/Pages/Dashboard/Payment/Payment.jsx
@@ -1,13 +1,22 @@
-
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
 
 const Payment = () => {
-    return (
-      <div className="w-full h-full" >
-        <div className=" w-full h-full flex items-center justify-center ">
+  const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
+  return (
+    <div className="w-full h-full">
+      <div className=" w-full h-full flex items-center justify-center ">
+        <div>
           <h2 className="text-4xl">Make Your Payment.</h2>
+          
+          <Elements stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Payment;
